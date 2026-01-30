31ace22cd1a02885bb4ef7931eaae0e76e79cae8
souzap371 <souzap371@gmail.com>
2026-01-30 13:34:40 -0300

    refatoração em forma de pagamento

--- src/main/java/com/expedicao/estoque/model/FormaPagamento.java
+++ src/main/java/com/expedicao/estoque/model/FormaPagamento.java
@@ -2,10 +2,15 @@ package com.expedicao.estoque.model;
 
 public enum FormaPagamento {
     PIX,
+    PIX_VIT,
+    CHEQUE,
+    CHEQUE_PRE,
     CC,
     CD,
     DEPOSITO,
+    DEPOSITO_VIT,
     ESPECIE,
+    GNRE,
     CARRO,
     MOTO,
     IMOVEL
